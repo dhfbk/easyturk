8b3109fc5f6cab3a8a4d25751c39adc690cf5fc5
valefras <48183554+valefras@users.noreply.github.com>
2020-09-09 09:55:17 +0200

    navBar, projectListItem added

---
+++ src/components/projectListItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="rounded shadow-lg m-4 p-4">
+        <span>{{ projectData.id }}</span>
+        <span>{{ projectData.title }}</span>
+        <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+        <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+        <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+        <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+        <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Button
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        projectData: Object,
+    },
+}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,12 @@
     <div class="home sm:mx-8 md:mx-16">
         <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
         <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-        <div class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-center ">
+        <div
+            class="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-center "
+        >
+            <div v-for="i in projects" :key="i.id" class="col-span-5">
+                <projectListItem :projectData="i" />
+            </div>
             <div class="rounded shadow-lg m-4 p-2">
                 <div class="font-bold text-xl  m-1">Saldo account</div>
                 <p class="text-gray-700 text-base m-1">
@@ -17,7 +22,7 @@
             </div>
 
             <div class="rounded  shadow-lg m-4 p-4 col-span-3 row-span-3	">
-                <div class="font-bold text-xl  m-1">Reviewable results</div>
+                <div class="font-bold text-lg  m-1">Workers</div>
                 <!-- component -->
                 <div class="container">
                     <!-- <h1 class="mb-8">
@@ -25,119 +30,101 @@
                     </h1> -->
 
                     <table class="text-left w-full">
-                        <thead class="bg-orange-400	 flex  w-full ">
-                            <tr class="flex w-full mb-4">
-                                <th class="p-4 w-1/4">One</th>
-                                <th class="p-4 w-1/4">Two</th>
-                                <th class="p-4 w-1/4">Three</th>
-                                <th class="p-4 w-1/4">Four</th>
+                        <thead class="bg-orange-500 text-white flex w-full ">
+                            <tr class="flex w-full">
+                                <th class="p-4 w-1/3">workerId</th>
+                                <th class="p-4 w-1/3">approvalRate</th>
+                                <th class="p-4 w-1/3">status</th>
                             </tr>
                         </thead>
                         <!-- Remove the nasty inline CSS fixed height on production and replace it with a CSS class — this is just for demonstration purposes! -->
                         <tbody
                             class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full h-64 border"
                         >
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full  ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
-                            <tr class="flex w-full mb-4">
-                                <td class="p-4 w-1/4">Dogs</td>
-                                <td class="p-4 w-1/4">Cats</td>
-                                <td class="p-4 w-1/4">Birds</td>
-                                <td class="p-4 w-1/4">Fish</td>
+                            <tr class="flex w-full ">
+                                <td class="p-4 w-1/3">workerId</td>
+                                <td class="p-4 w-1/3">approvalRate</td>
+                                <td class="p-4 w-1/3">status</td>
                             </tr>
                         </tbody>
                     </table>
@@ -155,11 +142,46 @@
 <script>
 // @ is an alias to /src
 //import HelloWorld from "@/components/HelloWorld.vue";
+import projectListItem from '../components/projectListItem'
 
 export default {
     name: 'Home',
     components: {
-        //HelloWorld
+        projectListItem,
+    },
+    data() {
+        return {
+            projects: [
+                {
+                    id: 'URJYRLU1PP',
+                    title: 'Offensive Tweet Recognition',
+                    date: '27/08/2018',
+                    hasGold: true,
+                    hasData: true,
+                },
+                {
+                    id: 'G7Q5EOBNDG',
+                    title: 'Image Tagging',
+                    date: '19/06/2018',
+                    hasGold: false,
+                    hasData: true,
+                },
+                {
+                    id: 'YQOKWUAP6A',
+                    title: 'Soccer Events Recognition',
+                    date: '04/12/2019',
+                    hasGold: false,
+                    hasData: true,
+                },
+                {
+                    id: '8GIS78FLI2',
+                    title: 'Survey on Sports',
+                    date: '30/05/2020',
+                    hasGold: false,
+                    hasData: false,
+                },
+            ],
+        }
     },
 }
 </script>
