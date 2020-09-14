031c8283065b56b2425e8a1e5b75ec06b797c7cf
valefras <48183554+valefras@users.noreply.github.com>
2020-09-14 08:51:11 +0200

    componente tabella risultati aggiunto

---
+++ src/components/resultsTable.vue
@@ -0,0 +1,93 @@
+<template>
+    <table class="w-full shadow-lg rounded">
+        <thead>
+            <tr class="text-center bg-orange-300 border-b border-grey uppercase">
+                <th class="px-2 py-2" @click="$emit('selectAll')">
+                    <span
+                        class="bg-white border-2 mx-auto rounded border-gray-400 w-5 h-5 flex flex-shrink-0 focus-within:border-blue-500"
+                    >
+                        <input type="checkbox" class="opacity-0 absolute" />
+                        <svg
+                            :class="!all ? '' : 'hidden'"
+                            class="fill-current w-4 h-4 text-green-500 pointer-events-none"
+                            viewBox="0 0 20 20"
+                        >
+                            <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+                        </svg>
+                    </span>
+                </th>
+                <th class="text-sm text-gray-700">HIT ID</th>
+                <th class="text-sm text-gray-700">Worker ID</th>
+                <th class="hidden sm:table-cell text-sm text-gray-700">
+                    Lifetime Approval Rate
+                </th>
+                <th class="hidden sm:table-cell text-sm text-gray-700">Date</th>
+                <th class="hidden sm:table-cell text-sm text-gray-700" @click="$emit('sort')">
+                    Status
+                    <svg
+                        class="inline"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="#4a5568"
+                        width="24px"
+                        height="24px"
+                    >
+                        <path d="M0 0h24v24H0V0z" fill="none" />
+                        <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" />
+                    </svg>
+                </th>
+            </tr>
+        </thead>
+        <tbody class="text-center">
+            <tr v-for="i in current" :key="i.HITId" class="border-b hover:bg-gray-100">
+                <td
+                    class="py-2 border-r"
+                    @click="i.selected = !i.selected"
+                    v-if="i.status == 'Submitted'"
+                >
+                    <span
+                        class="bg-white border-2 mx-auto rounded border-gray-400 w-5 h-5 flex flex-shrink-0 focus-within:border-blue-500"
+                    >
+                        <input type="checkbox" class="opacity-0 absolute" />
+                        <svg
+                            :class="i.selected ? '' : 'hidden'"
+                            class="fill-current w-4 h-4 text-green-500 pointer-events-none"
+                            viewBox="0 0 20 20"
+                        >
+                            <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
+                        </svg>
+                    </span>
+                </td>
+                <td class="py-2 border-r" v-else></td>
+                <td class="border-r py-2 text-sm text-gray-700 font-medium">
+                    {{ i.HITId }}
+                </td>
+                <td class="border-r py-2 text-sm text-gray-700 font-medium">
+                    {{ i.workerId }}
+                </td>
+                <td class="border-r py-2 text-sm text-gray-700 font-medium">
+                    {{ i.approvalRate }}
+                </td>
+                <td class="border-r py-2 text-sm text-gray-700 font-medium">
+                    {{ i.date }}
+                </td>
+                <td class="py-2 text-sm text-gray-700 font-medium">{{ i.status }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    props: {
+        current: Array,
+    },
+    data() {
+        return {
+            all: false,
+        }
+    },
+}
+</script>
+
+<style></style>
--- src/views/reviewResults.vue
+++ src/views/reviewResults.vue
@@ -43,90 +43,24 @@
                     </div>
                 </div>
             </div>
-
-            <table class="w-full shadow-lg rounded">
-                <thead>
-                    <tr class="text-center bg-orange-300 border-b border-grey uppercase">
-                        <th class="px-2 py-2" @click="selectAll(), (all = !all)">
-                            <span
-                                class="bg-white border-2 mx-auto rounded border-gray-400 w-5 h-5 flex flex-shrink-0 focus-within:border-blue-500"
-                            >
-                                <input type="checkbox" class="opacity-0 absolute" />
-                                <svg
-                                    :class="!all ? '' : 'hidden'"
-                                    class="fill-current w-4 h-4 text-green-500 pointer-events-none"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
-                                </svg>
-                            </span>
-                        </th>
-                        <th class="text-sm text-gray-700">HIT ID</th>
-                        <th class="text-sm text-gray-700">Worker ID</th>
-                        <th
-                            class="hidden sm:table-cell text-sm text-gray-700"
-                        >Lifetime Approval Rate</th>
-                        <th class="hidden sm:table-cell text-sm text-gray-700">Date</th>
-                        <th class="hidden sm:table-cell text-sm text-gray-700" @click="sort(mode)">
-                            Status
-                            <svg
-                                class="inline"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="#4a5568"
-                                width="24px"
-                                height="24px"
-                            >
-                                <path d="M0 0h24v24H0V0z" fill="none" />
-                                <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z" />
-                            </svg>
-                        </th>
-                    </tr>
-                </thead>
-                <tbody class="text-center">
-                    <tr v-for="i in current" :key="i.HITId" class="border-b hover:bg-gray-100">
-                        <td
-                            class="py-2 border-r"
-                            @click="i.selected = !i.selected"
-                            v-if="i.status == 'Submitted'"
-                        >
-                            <span
-                                class="bg-white border-2 mx-auto rounded border-gray-400 w-5 h-5 flex flex-shrink-0 focus-within:border-blue-500"
-                            >
-                                <input type="checkbox" class="opacity-0 absolute" />
-                                <svg
-                                    :class="i.selected ? '' : 'hidden'"
-                                    class="fill-current w-4 h-4 text-green-500 pointer-events-none"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path d="M0 11l2-2 5 5L18 3l2 2L7 18z" />
-                                </svg>
-                            </span>
-                        </td>
-                        <td class="py-2 border-r" v-else></td>
-                        <td class="border-r py-2 text-sm text-gray-700 font-medium">{{ i.HITId }}</td>
-                        <td class="border-r py-2 text-sm text-gray-700 font-medium">{{ i.workerId }}</td>
-                        <td
-                            class="border-r py-2 text-sm text-gray-700 font-medium"
-                        >{{ i.approvalRate }}</td>
-                        <td class="border-r py-2 text-sm text-gray-700 font-medium">{{ i.date }}</td>
-                        <td class="py-2 text-sm text-gray-700 font-medium">{{ i.status }}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <resultsTable :current="current" v-on:selectAll="selectAll()" v-on:sort="sort(mode)" />
             <div class="grid grid-cols-3 mt-2" v-if="data.length > numPerPage">
                 <button
                     @click="page--"
                     v-if="data.length > numPerPage && page >= 1"
                     class="bg-gray-300 w-48 hover:bg-gray-400 py-2 px-4 rounded m-2 focus:outline-none place-self-start"
-                >Previous</button>
+                >
+                    Previous
+                </button>
                 <div v-else></div>
                 <p class="text-center">Page {{ page + 1 }} of {{ pageNum + 1 }}</p>
                 <button
                     @click="page++"
                     v-if="data.length > numPerPage && page < pageNum"
                     class="bg-gray-300 w-48 hover:bg-gray-400 py-2 px-4 rounded m-2 focus:outline-none place-self-end"
-                >Next</button>
+                >
+                    Next
+                </button>
                 <div v-else></div>
             </div>
         </div>
@@ -134,7 +68,11 @@
 </template>
 
 <script>
+import resultsTable from '../components/resultsTable'
 export default {
+    components: {
+        resultsTable,
+    },
     watch: {
         page: function() {
             this.pageNum = Math.ceil(this.data.length / this.numPerPage) - 1
@@ -159,13 +97,6 @@ export default {
                 if (this.data[i]) {
                     this.current.push(this.data[i])
                 }
-                // console.log(i)
-                // console.log(
-                //     this.numPerPage,
-                //     this.page,
-                //     this.page * this.numPerPage + parseInt(this.numPerPage),
-                //     this.pageNum
-                // )
             }
         },
         onlyShow(x) {
@@ -185,19 +116,9 @@ export default {
             this.current = []
             this.currentPage()
         },
-        // trim(mode) {
-        //     if (mode == 'All') {
-        //         this.data = this.results
-        //     } else {
-        //         for (let i = 0; this.results.length; i++) {
-        //             if (this.results[i].status == mode) {
-        //                 this.data.push(this.results[i])
-        //                 console.log(this.data.length)
-        //             }
-        //         }
-        //     }
-        // },
+
         selectAll() {
+            this.all = !this.all
             for (let i = 0; i < this.data.length; i++) {
                 if (this.all == false) {
                     this.data[i].selected = false
@@ -217,7 +138,6 @@ export default {
             } else {
                 this.mode = 'Submitted'
             }
-            // console.log(this.data)
             this.current = []
             this.currentPage()
         },
@@ -231,7 +151,6 @@ export default {
         return {
             sortType: 'All',
             types: ['All', 'Submitted', 'Approved', 'Rejected'],
-            all: false,
             page: 0,
             numPerPage: 50,
             current: [],
