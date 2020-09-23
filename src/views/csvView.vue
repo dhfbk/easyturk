76e5d02d93778325a72c6265616320cd352d1dc2
valefras <48183554+valefras@users.noreply.github.com>
2020-09-23 17:19:19 +0200

    csv data view w/table and pagination added

--- src/views/csvView.vue
+++ src/views/csvView.vue
@@ -1,7 +1,115 @@
-<template><div></div></template>
+<template>
+    <div class=" flex items-center flex-col lg:w-5/6 mx-auto mt-4">
+        <p class="text-2xl text-primary mr-auto">{{ filename }}</p>
+        <div class="flex flex-row justify-start mr-auto mb-1">
+            <p class="text-md my-auto mr-1">Results per page:</p>
+            <input
+                class=" appearance-none h-full w-20 m-2 rounded border block appearance-none bg-white border-gray-400 text-gray-700 pl-2 pr-1 py-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                type="number"
+                id="quantity"
+                name="quantity"
+                min="10"
+                max="100"
+                step="10"
+                v-model="numPerPage"
+            />
+        </div>
+        <table class="shadow-md rounded w-full bg-white">
+            <thead>
+                <tr class="text-center text-white bg-primary border-b border-grey uppercase tracking-tight">
+                    <th v-for="x in headers" :key="x">
+                        {{ x }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(x, index) in current" :key="index" class="border-b hover:bg-gray-100">
+                    <td v-for="i in headers.length" :key="i">
+                        {{ x[i - 1] }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="grid grid-cols-3 mt-2" v-if="totalNum > numPerPage">
+            <button
+                @click="page--"
+                v-if="totalNum > numPerPage && page >= 1"
+                class="bg-gray-300 w-48 hover:bg-gray-400 py-2 px-4 rounded m-2 focus:outline-none place-self-start"
+            >
+                Previous
+            </button>
+            <div v-else></div>
+            <p class="text-center">Page {{ page + 1 }} of {{ pageNum + 1 }}</p>
+            <button
+                @click="page++"
+                v-if="totalNum > numPerPage && page < pageNum"
+                class="bg-gray-300 w-48 hover:bg-gray-400 py-2 px-4 rounded m-2 focus:outline-none place-self-end"
+            >
+                Next
+            </button>
+            <div v-else></div>
+        </div>
+    </div>
+</template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            current: [],
+            headers: [],
+            projectId: this.$route.params.projectId,
+            page: 0,
+            numPerPage: 50,
+            totalNum: 0,
+            filename: '',
+        }
+    },
+    created() {
+        console.log(this.projectId)
+        this.update()
+    },
+    methods: {
+        update() {
+            var url =
+                this.APIURL +
+                '?action=getData&id=' +
+                this.projectId +
+                '&howMany=' +
+                this.numPerPage +
+                '&page=' +
+                this.page
+            axios
+                .get(url)
+                .then(res => {
+                    console.log(res.data)
+                    this.current = res.data.data
+                    this.headers = res.data.fields
+                    this.totalNum = res.data.num
+                    this.pageNum = Math.ceil(this.totalNum / this.numPerPage) - 1
+                    this.filename = res.data.filename
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        },
+    },
+    watch: {
+        page: function() {
+            this.pageNum = Math.ceil(this.totalNum / this.numPerPage) - 1
+            this.current = []
+            this.update()
+        },
+        numPerPage: function() {
+            this.pageNum = Math.ceil(this.totalNum / this.numPerPage) - 1
+            this.page = 0
+            this.current = []
+            this.update()
+        },
+    },
+}
 </script>
 
 <style></style>
